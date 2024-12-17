ecc9e13a704872c73ae35ca6a3ff1bd356804fc2
Maksimov24K0157 <maksimobff@mail.ru>
2024-12-17 11:07:13 +0300

    Add files via upload

---
+++ #17.jl
@@ -0,0 +1,28 @@
+#17
+
+using HorizonSideRobots
+robot=Robot(animate=true)
+
+
+function spiral!(stop_condition::Function, robot)
+    nmax_steps = 1
+    s = Nord
+    while !find_direct!(stop_condition, robot, s, nmax_steps)
+        (s in (Nord, Sud)) && nmax_steps += 1
+        s = left(s)
+    end
+end
+function find_direct!(stop_condition::Function, robot, side, nmax_steps)
+    while !stop_condition() || n < nmax_steps
+        move!(robot, side)
+        n += 1
+    end
+    return stop_condition()
+end
+function find!(robot)
+    spiral!(()->ismarker(robot), robot)
+end
+
+
+
+left(side) = HorizonSide((Int(side)+1)%4)
