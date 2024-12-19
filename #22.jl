1559d272ba64d75a088e687f36b1157f8112a0c8
Maksimov24K0157 <maksimobff@mail.ru>
2024-12-19 11:26:28 +0300

    Add files via upload

---
+++ #22.jl
@@ -0,0 +1,30 @@
+
+#22
+using HorizonSideRobots
+
+robot = Robot(animate=true)
+
+function go!(robot, side, count)
+    if !isborder(robot, side)
+        move!(robot, side)
+        n = go!(robot, side, count + 1)
+    else
+        move!(robot, inverse(side), count)
+        n = move!(robot, inverse(side), count)
+    end
+    return n
+end
+
+function HorizonSideRobots.move!(robot, side, num_steps)
+    for i in 1:num_steps
+        if isborder(robot, side)
+            move!(robot, inverse(side), i - 1)
+            return false
+        else
+            move!(robot, side)
+        end
+    end
+    return true
+end
+
+inverse(side) = HorizonSide((Int(side) + 2) % 4)
